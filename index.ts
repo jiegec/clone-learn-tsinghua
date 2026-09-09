adb79e7770b88df0e9cbf4450c9c04bac0303b0b
Jiajie Chen <c@jia.je>
2026-09-09 23:26:28 +0800

    Trim class name

--- index.ts
+++ index.ts
@@ -468,7 +468,7 @@ async function callback(semester: { id: string, dirname: string }, course: Cours
                 let fileName = `${dir}/students.csv`;
                 let content = "student_id,real_name,gender,class_name,student_type,department,phone_number,email,nationality";
                 for (let student of students) {
-                    content += `\n${student.student_id},${student.real_name},${student.gender},${student.class_name},${student.student_type},${student.department},${student.phone_number},${student.email},${student.nationality}`;
+                    content += `\n${student.student_id},${student.real_name},${student.gender},${student.class_name.trim()},${student.student_type},${student.department},${student.phone_number},${student.email},${student.nationality}`;
                 }
                 fs.writeFileSync(fileName, content);
                 progress(`${course.chineseName}/students.csv Saved`);
